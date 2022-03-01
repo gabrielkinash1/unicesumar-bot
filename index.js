c3a8290ca4f8af6e7bc03083d7526f7e9c87ed54
Lucas <48861116+twChronous@users.noreply.github.com>
2022-03-01 11:09:55 -0300

    added notification system

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 require("dotenv").config();
 
 // Cliente do bot
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const ClassesData = require("./classes.json");
 
 const client = new Client({
     intents: [
@@ -13,21 +14,36 @@ const client = new Client({
     ],
 });
 
-// Canal de sugestões
 const SUGGESTIONS_CHANNEL = process.env.SUGGESTIONS_CHANNEL;
-
-// Token do bot
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const PING_CHANNEL = process.env.PING_CHANNEL;
 
-client.on("ready", () => {
+client.on("ready", (message) => {
+    const date = new Date();
+    const embed = new MessageEmbed();
+
     console.log("Bot está online!");
+
+    const isTodayAclassDay = Object.keys(ClassesData.conflitos).find(key => key === `${date.getDate()}/${date.getMonth() + 1}`);
+    if (isTodayAclassDay === `${date.getDate()}/${date.getMonth() + 1}`) {
+        return message.channels.cache.get(PING_CHANNEL).send({
+            embeds: [
+                embed
+                    .setColor("BLURPLE")
+                    .setTitle("Links para as aulas de hoje")
+                    .addField("Administração de conflitos e relacionamentos: ", ClassesData.conflitos[isTodayAclassDay])
+                    .addField("Fundamentos e Arquitetura de Computadores: ", ClassesData.arquitetura[isTodayAclassDay])
+            ]
+        })
+    }
 });
 
 client.on("messageCreate", async (message) => {
+    if(message.author.bot) return;
+
     if (message.channel.id === SUGGESTIONS_CHANNEL) {
         await message.react("👍");
         await message.react("👎");
     }
 });
-
 client.login(DISCORD_TOKEN);
